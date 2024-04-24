7388549f89294481265e66ad62b1533ca1a11547
unknown <estebangmontero@gmail.com>
2024-04-23 23:24:41 -0400

    front finalizado

--- dhemax-frontend/src/app/app.module.ts
+++ dhemax-frontend/src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapPipe } from './pipes/map.pipe';
+import { SortableHeaderDirective } from './directives/sortable-header.directive';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MapPipe, SortableHeaderDirective],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ dhemax-frontend/src/app/models/openMap.ts
@@ -0,0 +1,8 @@
+export interface OpenMap{
+    id: string;
+    estado: string;
+    operador: string;
+    nroConexiones: string;
+    coordenadas: string;
+    pais: string;
+}
